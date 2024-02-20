205de0d7d6d94c742c89921bfb710af2df4976a9
Andhikap <prameswaara@gmail.com>
2024-02-20 12:16:44 +0700

    consume data of user while logged in

--- src/main/java/com/prameswaradev/springsecuritypractice/WebController.java
+++ src/main/java/com/prameswaradev/springsecuritypractice/WebController.java
@@ -1,7 +1,11 @@
 package com.prameswaradev.springsecuritypractice;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class WebController {
@@ -10,14 +14,20 @@ public class WebController {
     public String publicPage(){
         return "hello Prameswara!";
     }
+
+//    to consume the data of user while log in
     @GetMapping(value = "/private")
-    public String privatePage(){
-        return "Hi welcome to our app!";
+    public String privatePage(Authentication authentication){
+        return "Hi welcome to our app king " + getName(authentication);
+    }
+
+    private String getName(Authentication authentication) {
+        return Optional.of(authentication.getPrincipal())
+                .filter(OidcUser.class::isInstance)
+                .map(OidcUser.class::cast)
+                .map(OidcUser::getFullName)
+                .orElseGet(authentication::getName);
     }
 
 
-
-
-
-    
 }
